94590cebce847901f0f44888c07dcea11584b943
Wil Stuckey <wstuckey@mozilla.com>
2024-10-31 15:12:04 -0500

    chore(plugin): be more specific on the glean config type. also format.

--- plugins/glean/src/index.ts
+++ plugins/glean/src/index.ts
@@ -10,35 +10,48 @@ import {
 
 import Glean from '@mozilla/glean/web';
 import GleanMetrics from '@mozilla/glean/metrics';
+import { ConfigurationInterface as GleanConfig } from '@mozilla/glean/dist/types/core/config';
 import { JsonObject } from '@backstage/types';
 import { create } from './metrics/backstage';
 
 interface DebugConfig extends JsonObject {
-  logging?: boolean,
-  tag?: string,
+  logging?: boolean;
+  tag?: string;
 }
 
 export class GleanAnalytics implements AnalyticsApi {
-  private constructor(appId: string, enabled: boolean, config: object, debug?: DebugConfig) {
+  private constructor(
+    appId: string,
+    enabled: boolean,
+    gleanConfig: GleanConfig,
+    debug?: DebugConfig,
+  ) {
     if (debug) {
       Glean.setLogPings(!!debug.logging);
       // set the debug tag if it is defined
       debug.tag && Glean.setDebugViewTag(debug.tag);
     }
-    Glean.initialize(appId, enabled, config);
+    Glean.initialize(appId, enabled, gleanConfig);
   }
 
   static fromConfig(config: ConfigApi): GleanAnalytics {
     const appId = config.getString('app.analytics.glean.appId');
     const enabled = config.getBoolean('app.analytics.glean.enabled');
-    const debug = config.getOptional('app.analytics.glean.debug') as DebugConfig;
+    const debug = config.getOptional(
+      'app.analytics.glean.debug',
+    ) as DebugConfig;
     const environment = config.getString('app.analytics.glean.environment');
 
-    return new GleanAnalytics(appId, enabled, {
-      enableAutoPageLoadEvents: false,
-      enabledAutoElementClickEvents: false,
-      channel: environment,
-    }, debug);
+    return new GleanAnalytics(
+      appId,
+      enabled,
+      {
+        enableAutoPageLoadEvents: false,
+        enableAutoElementClickEvents: false,
+        channel: environment,
+      },
+      debug,
+    );
   }
 
   captureEvent(event: AnalyticsEvent) {
@@ -55,14 +68,14 @@ export class GleanAnalytics implements AnalyticsApi {
         GleanMetrics.recordElementClick({
           id: event.attributes?.to.toString(),
           label: subject,
-        })
+        });
         break;
       case 'create':
         create.record({
           name: subject,
           entity_ref: event.attributes?.entityRef.toString(),
           time_saved: event.value,
-        })
+        });
         break;
 
       default:
