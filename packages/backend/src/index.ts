852857f4a0061592d2af6072276ce99443676809
André Honeiser <ahoneiser@mozilla.com>
2025-06-25 17:38:53 +0200

    chore: remove gcpIapAuth - we go with GitHub for now

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,7 +7,6 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
-import { gcpIapCustomAuthProvider } from './auth';
 
 const backend = createBackend();
 
@@ -21,7 +20,6 @@ backend.add(import('@backstage/plugin-techdocs-backend'));
 backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-backend.add(gcpIapCustomAuthProvider);
 // See https://backstage.io/docs/auth/guest/provider
 
 // catalog plugin
